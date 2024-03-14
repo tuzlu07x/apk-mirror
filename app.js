1484b925ee42a77f0f0d0fe8c7c5516cafb85daf
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2024-03-14 18:02:33 +0300

    appjs added.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Merhaba Dunya");
+import express from "express";
+import connectDB from "./config/db.js";
+import router from "./routes/router.js";
+const app = express();
+
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+console.log("sdfdsffdsfds");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server ${PORT} portunda başlatıldı`);
+});
